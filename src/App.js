1402181c3cc41a73a7b231cc416dd1725ff71e14
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-11-27 10:49:32 +0100

    lskvvsk

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ import { TextField, FlatButton } from 'material-ui';
 class App extends Component {
 
   state = {
-    task: [
+    tasks: [
       {taskName: 'Odkurzanie', completed: false},
       {taskName: 'Nakarmic kota', completed: false}
     ],
     taskName: ''
   }
-
+  handleChange= (event) => {
+    this.setState({ taskName: event.target.value })
+  }
+  handleClick= (event) => {
+    let tasks =this.state.tasks;
+    tasks.push({ taskName: this.state.taskName, completed: false })
+    this.setState({ tasks })
+  }
   render() {
     return (
       <div className="App">
         <div>
-            <TextField hintText="Enter your task here" />
-            <FlatButton label="Add" primary={true} />
+            <TextField hintText="Enter your task here" onChange={this.handleChange} />
+            <FlatButton label="Add" primary={true} onClick={this.handleClick} />
         </div>
         <div>
           {this.state.task.map((task, index) => (
