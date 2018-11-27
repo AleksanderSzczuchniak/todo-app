9189c387f39d96a856ddd22b57dce66fff95c168
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-11-27 10:34:59 +0100

    gesedf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import { TextField, FlatButton } from 'material-ui';
 
 
 class App extends Component {
+
+  state = {
+    task: [
+      {taskName: 'Odkurzanie', completed: false},
+      {taskName: 'Nakarmic kota', completed: false}
+    ],
+    taskName: ''
+  }
+
   render() {
     return (
       <div className="App">
+        <div>
+            <TextField hintText="Enter your task here" />
+            <FlatButton label="Add" primary={true} />
+        </div>
+        <div>
+          {this.state.task.map((task, index) => (
+            <div>{task.taskName}</div>
+          ))}
+        </div>
 
       </div>
     )
