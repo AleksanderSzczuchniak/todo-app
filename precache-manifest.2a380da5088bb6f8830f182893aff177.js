4cb3df00c55ea7db4b2019bb5ea8c500345a11dc
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-11-27 15:17:50 +0100

    Updates

---
+++ precache-manifest.2a380da5088bb6f8830f182893aff177.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "3c55577380d3ccdaf607",
+    "url": "/todo-app/static/js/main.3c555773.chunk.js"
+  },
+  {
+    "revision": "5d74e94ea1057af098e8",
+    "url": "/todo-app/static/js/1.5d74e94e.chunk.js"
+  },
+  {
+    "revision": "1996ab3e94ada0272c5257010e07285c",
+    "url": "/todo-app/index.html"
+  }
+];
